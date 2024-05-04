bd0e88ac1a38a968d2fa1b6e8938ba6264f5964f
002905 <miaoyu@zhejianglab.com>
2024-05-04 14:27:28 +0800

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,90 @@
+"use client";
+
 import Image from "next/image";
+import { useCallback, useEffect, useRef, useState } from "react";
+import Lottie, { AnimationItem } from "lottie-web";
+
+import "./index.css";
+
+const SHAKE_THRESHOLD = 15; // 阈值可能需要根据实际情况调整
 
 export default function Home() {
+  const [offset, setOffset] = useState(0);
+  const lottieRef = useRef<HTMLDivElement>(null);
+  const [lottieInstance, setLottieInstance] = useState<AnimationItem>();
+  const [canUseShake, setCanUseShake] = useState(false);
+  const last = useRef({
+    last_x: -Infinity,
+    last_y: -Infinity,
+    last_z: -Infinity,
+    last_update: 0,
+  });
+  const handleShake = useCallback(() => {
+    setOffset(offset + 10 > 80 ? 80 : offset + 10);
+    lottieInstance?.show();
+    lottieInstance?.play();
+    lottieInstance?.addEventListener("loopComplete", () => {
+      lottieInstance.hide();
+    });
+  }, [offset, lottieInstance, setOffset]);
+
+  const deviceMotionHandler = useCallback(
+    (eventData: DeviceMotionEvent) => {
+      const acceleration = eventData.accelerationIncludingGravity!;
+      const { x, y, z } = acceleration || {};
+      const curTime = new Date().getTime();
+      const eventHasData = x && y && z;
+      const { last_update, last_x, last_y, last_z } = last.current;
+      if (curTime - last_update > 100 && eventHasData) {
+        const diffTime = curTime - last_update;
+        const speed =
+          (Math.abs(x + y + z - last_x - last_y - last_z) / diffTime) * 10000;
+        if (speed > SHAKE_THRESHOLD) {
+          // 判断为摇一摇动作
+          handleShake();
+        }
+        last.current = {
+          last_x: x,
+          last_y: y,
+          last_z: z,
+          last_update: curTime,
+        };
+      }
+    },
+    [handleShake]
+  );
+
+  useEffect(() => {
+    if (window.DeviceMotionEvent) {
+      setCanUseShake(true);
+      // 支持，可以继续
+      window.addEventListener("devicemotion", deviceMotionHandler, false);
+    } else {
+      alert("该浏览器不支持摇一摇功能");
+      setCanUseShake(false);
+    }
+  }, [deviceMotionHandler, setCanUseShake]);
+
+  useEffect(() => {
+    if (!lottieRef.current) {
+      return;
+    }
+    const instance = Lottie.loadAnimation({
+      container: lottieRef.current, // the dom element that will contain the animation
+      renderer: "svg",
+      loop: true,
+      autoplay: false,
+      path: "/celebrate.json", // the path to the animation json
+    });
+    instance.hide();
+    setLottieInstance(instance);
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center p-6">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+          用力摇一摇&nbsp;
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
@@ -15,99 +93,36 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+            期待您的到来
           </a>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+      <div className="mb-16 flex items-center justify-between w-full">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
+          src="/boy-left.png"
+          alt="Boy"
+          className="dark:invert"
+          width={100}
+          height={100}
           priority
+          style={{ transform: `translateX(${offset}px)` }}
+        />
+        <Image
+          src="/girl-right.png"
+          alt="Girl"
+          className="dark:invert"
+          width={100}
+          height={100}
+          priority
+          style={{ transform: `translateX(-${offset}px)` }}
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <div ref={lottieRef} className="lottie-container"></div>
+      {canUseShake ? null : (
+        <button onClick={handleShake} style={{ zIndex: 9999 }}>
+          点我
+        </button>
+      )}
     </main>
   );
 }
